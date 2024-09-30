53be93eeeef70eaf962810eb6c803757705c958c
Samuel Pacheco <samuelpachecoarias@gmail.com>
2024-09-29 23:55:05 -0500

    Cursos con estudiantes

--- curso/models.py
+++ curso/models.py
@@ -1,10 +1,14 @@
 # models.py
 from django.db import models
 
+from persona.models import Persona
+
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     duracion = models.IntegerField()  # Duración en horas
-
+    
+    estudiantes = models.ManyToManyField(Persona, related_name='curso', blank=True)
+    
     def __str__(self):
         return self.nombre
--- curso/views.py
+++ curso/views.py
@@ -1,5 +1,13 @@
-
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Curso
 from django.shortcuts import render
 
 def get_curso(request):
-    return render (request, 'curso.html')
+    
+    Cursos = Curso.objects.values()
+    print(Cursos.estudiantes.values())
+    
+    return render (request, 'curso.html',{'title': 'Lista de estudiantes',
+        'curso': Cursos})
+
